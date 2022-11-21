e3d6085a4e301768a5588ee43d7cc10bae307b56
Denis Salimov <denis_45@bk.ru>
2022-11-21 23:13:33 +0500

    Make this a little bit easier

--- randomgame.py
+++ randomgame.py
@@ -2,7 +2,7 @@ import random
 
 while True:
     start_input = input('Hey, this is my first simple game, please enter you number from 1~3: ')
-    random_num = str(random.randint(1, 3))
+    random_num = str(random.randint(1, 2))
     if start_input == random_num:
         print('WOOOOOWWW, you are win!!!! OMGGGGGGGG')
         print(f'The number was really {start_input} !!!')
