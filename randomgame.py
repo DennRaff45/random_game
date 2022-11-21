f4b64033d26d6ab58b5ba3f1f6713a5f73a496e0
Denis Salimov <denis_45@bk.ru>
2022-11-21 23:10:27 +0500

    Pushing the random game

---
+++ randomgame.py
@@ -0,0 +1,12 @@
+import random
+
+while True:
+    start_input = input('Hey, this is my first simple game, please enter you number from 1~3: ')
+    random_num = str(random.randint(1, 3))
+    if start_input == random_num:
+        print('WOOOOOWWW, you are win!!!! OMGGGGGGGG')
+        print(f'The number was really {start_input} !!!')
+        break
+    elif start_input != random_num:
+        print('mmmmmm.... try again, I think you are so close....')
+        print('The number was: ' + str(random_num))
